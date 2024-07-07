43b50f03646793973d6976d9aeb7f0a2e8c41bad
Alireza Joz <alireza00joz@gmail.com>
2024-07-07 07:54:47 +0330

    is file mangment  reatoin is login

--- routes/admin.php
+++ routes/admin.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 //---------------------------------------HomePage---------------------------------------//
-Route::get('/', App\Livewire\Admin\Home\Index::class)->name('admin.home');
+Route::get('/', App\Livewire\Admin\Home\Index::class)->name('admin');
 
 // //---------------------------------------Settings---------------------------------------//
 // //Settings Footer ETC
